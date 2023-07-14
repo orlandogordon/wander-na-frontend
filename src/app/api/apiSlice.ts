cb0ee9fbabbbf60f3ef169ff5e43bdec9dca53d6
Orlando Gordon <ogordon1998@gmail.com>
2023-07-14 08:29:31 -0400

    update headers for all requeests to fix issue with cors

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,7 +1,13 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: 'https://wander-api-bl56.onrender.com', credentials: 'include'}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://wander-api-bl56.onrender.com', 
+        credentials: 'include',  
+        prepareHeaders: (headers) => {
+        headers.set('Access-Control-Allow-Origin', 'https://wander-xggp.onrender.com/')
+        return headers
+    }}),
     tagTypes: ['Tour', 'User'],
     endpoints: builder => ({})
 })
