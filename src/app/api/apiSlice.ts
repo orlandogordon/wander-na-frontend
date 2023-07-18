63890d56440a237908bfbc52d5afa705a7895ec9
Orlando Gordon <ogordon1998@gmail.com>
2023-07-17 21:02:27 -0400

    Testing issue with jwt applying to cookies.

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -4,10 +4,7 @@ export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://wander-api-bl56.onrender.com', 
         credentials: 'include',  
-        prepareHeaders: (headers) => {
-        headers.set('Access-Control-Allow-Origin', 'https://wander-xggp.onrender.com/')
-        return headers
-    }}),
+    }),
     tagTypes: ['Tour', 'User'],
     endpoints: builder => ({})
 })
--- src/features/auth/Login/index.tsx
+++ src/features/auth/Login/index.tsx
@@ -34,12 +34,13 @@ const Login = (props: Props) => {
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     try {
-      const { token: accessToken } = await login({ email, password }).unwrap();
+      const data = await login({ email, password }).unwrap();
+      const { token: accessToken } = data;
       dispatch(setCredentials({ accessToken }));
       setEmail("");
       setPassword("");
       navigate("/");
-      navigate(0);
+      //navigate(0);
     } catch (err: any) {
       if (!err.status) {
         setErrMsg("No Server Response");
